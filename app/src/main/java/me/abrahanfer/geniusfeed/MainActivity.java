a2a6b423fe550df1f7e9d929a8e67edbefd27c15
Abrahán Fernández Nieto <abrahanfer@gmail.com>
2015-09-29 21:39:31 +0200

    Add changes to return feed items

--- app/src/main/java/me/abrahanfer/geniusfeed/MainActivity.java
+++ app/src/main/java/me/abrahanfer/geniusfeed/MainActivity.java
@@ -54,31 +54,12 @@ public class MainActivity extends ActionBarActivity {
         // Make a request to API
         // Instantiate the RequestQueue.
 
-        class RetrieveSomething extends AsyncTask<String, Void, String> {
+        class GetFeedItemsUnread extends AsyncTask<String, Void, FeedItemRead[]> {
             // RequestQueue queue = Volley.newRequestQueue(this);
             private Exception exception;
 
-            protected String doInBackground(String...urls) {
-                String url = "http://192.168.1.55/feed_item_reads.json";
+            protected FeedItemRead[] doInBackground(String...urls) {
 
-                // Request a string response from the provided URL.
-                /*JsonObjectRequest jsonRequest = new JsonObjectRequest(url,null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        TextView mTextView =(TextView) findViewById(R.id.textTest);
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: "+ response.toString());
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                TextView mTextView =(TextView) findViewById(R.id.textTest);
-                mTextView.setText("That didn't work!" + error.networkResponse.data);
-            }
-        });
-        // Add the request to the RequestQueue.
-        queue.add(jsonRequest);*/
                 // Adding header for Basic HTTP Authentication
                 HttpAuthentication authHeader = new HttpBasicAuthentication("test-user-1", "test1");
                 HttpHeaders requestHeaders = new HttpHeaders();
@@ -93,18 +74,23 @@ public class MainActivity extends ActionBarActivity {
 
                         );
 
-                HttpEntity<FeedItemReadDRResponse> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, FeedItemReadDRResponse.class);
+                HttpEntity<FeedItemReadDRResponse> response;
+                response = restTemplate.exchange(urls[0], HttpMethod.GET, requestEntity, FeedItemReadDRResponse.class);
 
                 FeedItemReadDRResponse result = response.getBody();
                 System.out.println("Array size: " + result.getResults().length);
-                FeedItemReadDRResponse djangoRestResponse = restTemplate.getForObject(url, FeedItemReadDRResponse.class);
-                    System.out.println(djangoRestResponse.getCount());
+
+                FeedItemReadDRResponse djangoRestResponse;
+                djangoRestResponse = restTemplate.getForObject(urls[0], FeedItemReadDRResponse.class);
+
+                System.out.println(djangoRestResponse.getCount());
                 FeedItemRead[] feedItemReads = djangoRestResponse.getResults();
                 System.out.println("Array size: " + feedItemReads.length);
-                return new String();
+                return feedItemReads;
             }
         };
 
-        new RetrieveSomething().execute("mierda");
+        String url = "http://192.168.1.55/feed_item_reads/unread.json";
+        FeedItemRead[] feedItemsUnread = new GetFeedItemsUnread().execute(url);
     }
 }
