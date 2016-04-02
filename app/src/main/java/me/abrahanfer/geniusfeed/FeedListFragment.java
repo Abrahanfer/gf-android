9f9afa438cba946949e7dac4db89410833e3384d
Abrahán Fernández Nieto <abrahanfer@gmail.com>
2016-04-02 16:02:18 +0200

    Add NavigationView and App Style

---
+++ app/src/main/java/me/abrahanfer/geniusfeed/FeedListFragment.java
@@ -0,0 +1,19 @@
+package me.abrahanfer.geniusfeed;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created by abrahan on 2/04/16.
+ */
+public class FeedListFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.feed_list_fragment,
+                                container, false);
+    }
+}
--- app/src/main/java/me/abrahanfer/geniusfeed/MainActivity.java
+++ app/src/main/java/me/abrahanfer/geniusfeed/MainActivity.java
@@ -1,9 +1,19 @@
 package me.abrahanfer.geniusfeed;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,42 +40,113 @@ import me.abrahanfer.geniusfeed.models.FeedItemRead;
 import me.abrahanfer.geniusfeed.utils.FeedArrayAdapter;
 import me.abrahanfer.geniusfeed.utils.Authentication;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private NavigationView mNvDrawer;
+
     public final static String FEED = "me.abrahanfer.geniusfeed" +
             ".FEED";
-    public final static String LOGIN_CREDENTIALS = "me.abrahanfer.geniusfeed" +
-            ".LOGIN_CREDENTIALS";
-   // public final static String DOMAIN = "10.0.240.29";
-   public final static String DOMAIN = "192.168.1.55";
+    public final static String LOGIN_CREDENTIALS =
+            "me.abrahanfer.geniusfeed" + ".LOGIN_CREDENTIALS";
+    // public final static String DOMAIN = "10.0.240.29";
+    public final static String DOMAIN = "192.168.1.55";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupListFeeds();
-        testRequest();
+
+        // Set a Toolbar to replace the ActionBar.
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable
+                                                           .ic_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerLayout = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+
+        // Find our drawer view
+        mNvDrawer = (NavigationView) findViewById(
+                R.id.navigationView);
+
+        // Setup drawer view
+        setupDrawerContent(mNvDrawer);
+
+
+
+
+
+        View headerLayout = mNvDrawer.getHeaderView(0);
+        // Custom methods to populate list of feeds
+        // setupListFeeds();
+        // testRequest();
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(
+                            MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_feed_list:
+                fragmentClass = FeedListFragment.class;
+                break;
+            default:
+                fragmentClass = FeedListFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                       .replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawerLayout.closeDrawers();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -73,65 +154,76 @@ public class MainActivity extends ActionBarActivity {
         // Make a request to API
         // Instantiate the RequestQueue.
 
-        class RetrieveFeeds extends AsyncTask<String, Void,
-                Feed[]> {
+        class RetrieveFeeds extends AsyncTask<String, Void, Feed[]> {
             private Exception exception;
 
             @Override
-            protected Feed[] doInBackground(String...urls) {
+            protected Feed[] doInBackground(String... urls) {
 
-                Authentication authentication = Authentication.getCredentials();
-                if(authentication == null) {
-                    Intent intent = new Intent(getApplicationContext(), LoginActivity
-                            .class);
+                Authentication authentication = Authentication
+                        .getCredentials();
+                if (authentication == null) {
+                    Intent intent = new Intent(
+                            getApplicationContext(),
+                            LoginActivity.class);
 
 
                     startActivity(intent);
                     return new Feed[0];
-                }else {
+                } else {
                     String username = authentication.getUsername();
                     String password = authentication.getPassword();
 
 
                     // Adding header for Basic HTTP Authentication
-                    HttpAuthentication authHeader = new HttpBasicAuthentication(username, password);
+                    HttpAuthentication authHeader
+                            = new HttpBasicAuthentication(username,
+                                                          password);
                     HttpHeaders requestHeaders = new HttpHeaders();
                     requestHeaders.setAuthorization(authHeader);
-                    HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
+                    HttpEntity<?> requestEntity
+                            = new HttpEntity<Object>(requestHeaders);
 
                     RestTemplate restTemplate = new RestTemplate();
 
                     restTemplate.getMessageConverters()
-                            .add(new MappingJackson2HttpMessageConverter());
+                                .add(new MappingJackson2HttpMessageConverter());
                     try {
-                        HttpEntity<FeedDRResponse> response = restTemplate
-                                .exchange(urls[0], HttpMethod.GET, requestEntity,
-                                        FeedDRResponse.class);
+                        HttpEntity<FeedDRResponse> response
+                                = restTemplate
+                                .exchange(urls[0], HttpMethod.GET,
+                                          requestEntity,
+                                          FeedDRResponse.class);
 
                         FeedDRResponse result = response.getBody();
                         Feed[] feeds = result.getResults();
-                        System.out.println("Array size: " + feeds.length);
+                        System.out.println(
+                                "Array size: " + feeds.length);
 
                         return feeds;
                     } catch (RestClientException springException) {
-                        System.out.println("Mirando la excepcion de Spring");
+                        System.out.println(
+                                "Mirando la excepcion de Spring");
                         System.out.println(springException);
                         return new Feed[0];
                     }
                 }
             }
 
-            protected void onPostExecute(Feed[] feeds){
-                System.out.println("Mirando en onPostExecute 2" + feeds + "Tantos feed");
-                ArrayList<Feed> feedArrayList = new
-                        ArrayList<>(Arrays.asList(feeds));
+            protected void onPostExecute(Feed[] feeds) {
+                System.out.println(
+                        "Mirando en onPostExecute 2" + feeds +
+                                "Tantos feed");
+                ArrayList<Feed> feedArrayList = new ArrayList<>(
+                        Arrays.asList(feeds));
 
-                ListView listFeeds =(ListView) findViewById(R.id.listFeeds);
-                FeedArrayAdapter feedArrayAdapter = new
-                        FeedArrayAdapter(getApplicationContext(),
-                        feedArrayList);
+               /* ListView listFeeds = (ListView) findViewById(
+                        R.id.listFeeds);
+               / FeedArrayAdapter feedArrayAdapter
+                        = new FeedArrayAdapter(
+                        getApplicationContext(), feedArrayList);
 
-                listFeeds.setAdapter(feedArrayAdapter);
+                listFeeds.setAdapter(feedArrayAdapter);*/
             }
         }
 
@@ -141,27 +233,28 @@ public class MainActivity extends ActionBarActivity {
         new RetrieveFeeds().execute(url);
     }
 
-    public void setupListFeeds(){
-        final ListView listFeeds =(ListView) findViewById(R.id.listFeeds);
+    public void setupListFeeds() {
+       /* final ListView listFeeds = (ListView) findViewById(
+                R.id.listFeeds);
 
         listFeeds.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
-                    public void onItemClick(AdapterView<?> parent, View view,
-                                            int position, long id) {
+                    public void onItemClick(AdapterView<?> parent,
+                                            View view, int position,
+                                            long id) {
                         System.out.println("Feed click" + position);
-                        Intent intent = new Intent(getApplicationContext(),FeedActivity
-                                .class);
-                        Feed feed =(Feed) listFeeds
-                                .getAdapter()
-                                .getItem
-                                (position);
-                        intent.putExtra(FEED, feed
-                                .getLink().toString());
+                        Intent intent = new Intent(
+                                getApplicationContext(),
+                                FeedActivity.class);
+                        Feed feed = (Feed) listFeeds.getAdapter()
+                                                    .getItem(
+                                                            position);
+                        intent.putExtra(FEED,
+                                        feed.getLink().toString());
 
                         startActivity(intent);
                     }
-                }
-        );
+                });*/
     }
 }
